c46ea8998d6ca3a41b99cb345552166ac724e067
Spidercracknull <jaimes.jaramillo.santiago@gmail.com>
2023-03-20 00:03:09 -0600

    primer comentario

---
+++ src/java/Modelo/ClaseCliente.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelo;
+
+
+/**
+ *
+ * @author alumno
+ */
+public class ClaseCliente {
+
+    public String nombre;
+    public double telefono;
+    public String correo;
+    
+    public ClaseCliente() {}
+    
+    public ClaseCliente(String nombre, double telefono, String correo) {
+        this.nombre = nombre;
+        this.telefono = telefono;
+        this.correo = correo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(double telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+    
+
+    
+}
